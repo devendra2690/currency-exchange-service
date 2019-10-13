62642ad07b029cc469b82fdc15cf022c9e738ad7
devendra2690 <devendra_patil26@yahoo.com>
2019-10-13 13:03:55 +0530

    Changing bean attribute

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -17,51 +17,61 @@ public class CurrencyExchange {
 	private int serverPort;
 
 	
-	public CurrencyExchange(String from, String to, int exchangeRate, int serverPort) {
+	public CurrencyExchange(int id, String fromCurrency, String toCurrency, int exchangeRate, int serverPort) {
 		super();
-		this.fromCurrency = from;
-		this.toCurrency = to;
+		this.id = id;
+		this.fromCurrency = fromCurrency;
+		this.toCurrency = toCurrency;
 		this.exchangeRate = exchangeRate;
 		this.serverPort = serverPort;
 	}
-	
+
 	//Need this default constructor when working with JPA	
 	// Error : org.hibernate.InstantiationException: No default constructor for entity:  : com.in28minutes.microservices.currencyexchangeservice.CurrencyExchange
 	public CurrencyExchange() {
 	
 	}
 
+	public String getFromCurrency() {
+		return fromCurrency;
+	}
 
+	public void setFromCurrency(String fromCurrency) {
+		this.fromCurrency = fromCurrency;
+	}
 
+	public String getToCurrency() {
+		return toCurrency;
+	}
 
+	public void setToCurrency(String toCurrency) {
+		this.toCurrency = toCurrency;
+	}
+
+	public int getExchangeRate() {
+		return exchangeRate;
+	}
+
+	public void setExchangeRate(int exchangeRate) {
+		this.exchangeRate = exchangeRate;
+	}
 
 	public int getServerPort() {
 		return serverPort;
 	}
+
 	public void setServerPort(int serverPort) {
 		this.serverPort = serverPort;
 	}
-	public String getFrom() {
-		return fromCurrency;
+
+	public int getId() {
+		return id;
 	}
-	public void setFrom(String from) {
-		this.fromCurrency = from;
-	}
-	public String getTo() {
-		return toCurrency;
-	}
-	public void setTo(String to) {
-		this.toCurrency = to;
-	}
-	public int getExchangeRate() {
-		return exchangeRate;
-	}
-	public void setExchangeRate(int exchangeRate) {
-		this.exchangeRate = exchangeRate;
-	}
-	
+
 	@Override
 	public String toString() {
-		return "CurrencyExchange [from=" + fromCurrency + ", to=" + toCurrency + ", exchangeRate=" + exchangeRate + "]";
+		return "CurrencyExchange [id=" + id + ", fromCurrency=" + fromCurrency + ", toCurrency=" + toCurrency
+				+ ", exchangeRate=" + exchangeRate + ", serverPort=" + serverPort + "]";
 	}
+		
 }
