e2eb7f5f99d7559f73f2b433e9bf9fa943e1dd02
devendra2690 <devendra_patil26@yahoo.com>
2019-11-20 01:07:53 +0530

    Zipkin INtroduction

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,12 +1,18 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class CurrencyExchangeController {
+	
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 
 	@Autowired
 	org.springframework.core.env.Environment env;
@@ -19,6 +25,8 @@ public class CurrencyExchangeController {
 		
     	CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromCurrencyAndToCurrency(from, to);
     	currencyExchange.setServerPort(Integer.parseInt(env.getProperty("local.server.port")));
+    	
+    	logger.info("{}",currencyExchange);
 		
     	return currencyExchange;
 	}
